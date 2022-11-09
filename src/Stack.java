5181ef8da1500389d6f86480593ace9373cf67de
eric.peronett <eric.peronetti@heig-vd.ch>
2022-11-09 15:16:18 +0100

    Initial files

---
+++ src/Stack.java
@@ -0,0 +1,26 @@
+public class Stack<T> {
+    public Stack() {
+
+    }
+
+    public void push(T obj) {
+
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    public Object[] toArray() {
+        return null;
+    }
+
+    /*public Iterator<T> iterator() {
+
+    }*/
+}
---
+++ src/StackElement.java
@@ -0,0 +1,11 @@
+public class StackElement<T> {
+    private T next;
+
+    public boolean hasNext() {
+        return true;
+    }
+
+    public T next() {
+        return null;
+    }
+}
