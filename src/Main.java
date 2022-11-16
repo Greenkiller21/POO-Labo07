2bbc9ca175e1e1253e9c7301e9ef36a753aa9eef
eric.peronett <eric.peronetti@heig-vd.ch>
2022-11-16 15:24:33 +0100

    Main ok + Commentaires hanoi

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-import hanoi.CmdHanoiDisplayer;
 import hanoi.Hanoi;
-import hanoi.HanoiDisplayer;
 import hanoi.gui.JHanoi;
 
 public class Main {
     public static void main(String[] args) {
-        Hanoi h = new Hanoi(3, new CmdHanoiDisplayer());
-        h.solve();
-
-        JHanoi h2 = new JHanoi();
+        if (args.length == 0) {
+            new JHanoi();
+        }
+        if (args.length == 1) {
+            int nbDisk;
+            try {
+                nbDisk = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("The number entered was not valid !");
+            }
+            Hanoi h = new Hanoi(nbDisk);
+            h.solve();
+        }
     }
 }
